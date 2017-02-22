5733314f031b80908cf65f53ad5c302f954acdb0
TimothyMcWatters <timmch2o@gmail.com>
2017-02-21 20:43:25 -0600

    Fixed code to accept doubles

--- Sources/FutureValues.swift
+++ Sources/FutureValues.swift
@@ -1,24 +1,17 @@
 import Foundation
 
-//struct FutureValues {
+//: # FUTURE VALUE CALCULATOR
     
-    //: # FUTURE VALUE CALCULATOR
-    
-    /*:
-     ## FUNCTION TO PERFORM FUTURE VALUES CALCULATIONS.
-     * param p  The principal deposit.
-     * param r  The period interest rate (in this case annual).
-     * param t  Length of time (period) invested in years.
-     * prints:  value resultent from furture values formula.
-     */
-    public func calculateFutureValue(p: Int, r: Double, t: Int) {
-        let doubleP:Double = Double(p)
-        let doubleT:Double = Double(t)
-        let resultString: String
-        resultString = String(format: "%.2f", round(100 * (doubleP * (pow((1 + r), doubleT))))/100)
-        print("Your future value after \(t) years is is $\(resultString)")
-    } // End of calculateFutureValue
-    
-    //calculateFutureValue(p: 10000, r: 0.06, t: 3)
-    
-//}
+/*:
+## FUNCTION TO PERFORM FUTURE VALUES CALCULATIONS.
+* param p  The principal deposit.
+* param r  The period interest rate (in this case annual).
+* param t  Length of time (period) invested in years.
+* prints:  value resultent from furture values formula.
+*/
+public func calculateFutureValue(p: Double, r: Double, t: Double) {
+    let resultString: String
+    resultString = String(format: "%.2f", round(100 * (p * (pow((1 + r), t))))/100)
+    print("Your future value after \(t) years is is $\(resultString)")
+} // End of calculateFutureValue
+
